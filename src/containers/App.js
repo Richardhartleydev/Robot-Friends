1067c34b0d7941e163396503cf9d4c5a2b3d0ee9
richardhartleydev <richardhartleydev@gmail.com>
2023-10-15 18:38:57 +0100

    Refactor: Changes class components to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,60 +1,48 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
-// Old way of doing React classes but good to know for 1st project
-class App extends Component {
-  constructor() {
-    // Cant run constructor without super()
-    super();
-    // Set state
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+// Set state
+export default function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
   // Fetch user data from url
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       // Return that data in the form of JSON
       .then(response => response.json())
       // Set robots empty array state to user data
-      .then(users => this.setState({ robots: users }));
-  }
+      .then(users => setRobots(users));
+    // useEffect() takes an array as a 2nd arg so it doesn't keep re-rendering
+  }, []);
 
-  // A function that sets searchfield empty string state to searchfield.value
-  onSearchChange = event => {
-    this.setState({ searchfield: event.target.value });
+  // Set searchfield empty string state to searchfield.value
+  const onSearchChange = event => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    // destructuring to not repeat "this.state" lots in the code
-    const { robots, searchfield } = this.state;
-    // filter users on page based on updated searchfield state
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
+  // Filter users on page based on updated searchfield state
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
 
-    // If fetch request fails or takes too long, display this msg
-    // Can add loading GIF in future
-    return !robots.length ? (
-      <h1 className="tc">Loading or Failed API Request</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-  }
+  // If fetch request fails or takes too long, display this msg
+  // Can add loading GIF in future
+  return !robots.length ? (
+    <h1 className="tc">Loading or Failed API Request</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
 }
-
-export default App;
